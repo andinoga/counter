04e855dd7bf04259ea3583239893d2c898f94fab
Gloria Andino <andinoga@gmail.com>
2019-01-28 04:01:53 +0000

    First commit

---
+++ src/js/index.js
@@ -0,0 +1,52 @@
+//import react into the bundle
+import React from "react";
+import ReactDOM from "react-dom";
+import PropTypes from "prop-types";
+
+//include bootstrap npm library into the bundle
+import "bootstrap";
+
+//include your index.scss file into the bundle
+import "../styles/index.scss";
+
+function SimpleCounter(props) {
+	return (
+		<div className="bigCounter">
+			<div className="calendar">
+				<i className="far fa-clock"> </i>
+			</div>
+			<div className="four">{props.digitFour % 10}</div>
+			<div className="three">{props.digitThree % 10}</div>
+			<div className="two">{props.digitTwo % 10}</div>
+			<div className="one">{props.digitOne % 10}</div>
+		</div>
+	);
+}
+
+SimpleCounter.propTypes = {
+	// You can declare that a prop is a specific JS primitive. By default, these
+	// are all optional.
+	digitFour: PropTypes.array,
+	digitThree: PropTypes.array,
+	digitTwo: PropTypes.array,
+	digitOne: PropTypes.array
+};
+
+let counter = 0;
+setInterval(function() {
+	const four = Math.floor(counter / 10000);
+	const three = Math.floor(counter / 100);
+	const two = Math.floor(counter / 10);
+	const one = Math.floor(counter / 1);
+
+	counter++;
+	ReactDOM.render(
+		<SimpleCounter
+			digitOne={one}
+			digitTwo={two}
+			digitThree={three}
+			digitFour={four}
+		/>,
+		document.querySelector("#app")
+	);
+}, 1000);
